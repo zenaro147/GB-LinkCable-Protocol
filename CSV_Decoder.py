b2e77a3f14aef0bc465f931373a671abfa1fe974
Rafael Zenaro <rzoliveira93@gmail.com>
2022-01-31 11:51:36 -0300

    WIP CSV Decoder to HEX

--- CSV_Decoder.py
+++ CSV_Decoder.py
@@ -1,7 +1,17 @@
+import os
 import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+
+#Open a dialog to select the CSV file
+root = tk.Tk()
+root.withdraw()
+my_dir = os.getcwd()
+pattern = (('CSV', '*.csv'),)
+file_path = filedialog.askopenfilename(initialdir=my_dir, filetypes=pattern)
 
 #Processing the file
-raw_df = pd.read_csv (r'C:\Users\zenaror\OneDrive - Banco Itaú SA\Documents\Print Picture.csv', sep='\t')
+raw_df = pd.read_csv (file_path, sep='\t')
 clk0_df = raw_df.loc[(raw_df['CLK'] == 0)]
 
 bin_concat=""
